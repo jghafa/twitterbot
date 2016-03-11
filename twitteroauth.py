4db9a3af2ed44294aa281247293bc136c3a27d93
jghafa <james.haley@yahoo.com>
2016-03-11 16:58:28 -0500

    read tweets a specific users and find specific phrase

--- twitteroauth.py
+++ twitteroauth.py
@@ -16,8 +16,7 @@ api = tweepy.API(auth)
 user = api.me()
  
 print('Name: ' + user.name)
-print('Location: ' + user.location)
-print('Friends: ' + str(user.friends_count))
+print ()
 
 # Get the User object for twitter...
 user = api.get_user('jameshaley')
@@ -25,3 +24,11 @@ print (user.screen_name)
 print (user.followers_count)
 for friend in user.friends():
    print (friend.screen_name)
+print()
+
+tweets = api.user_timeline(screen_name = 'SpaceX', count = 100)
+
+for chirp in tweets:
+    #print (dir(chirp))
+    if '@elonmusk' in chirp.text:
+        print (chirp.text)
