63ce8288862b56edf650a6ed8d31802fc9fcc754
jghafa <james.haley@yahoo.com>
2016-03-12 22:05:33 -0500

    write SQL, show Twitter Rate Limit

--- twitteroauth.py
+++ twitteroauth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 import tweepy
 import os
-#from tweepy import OAuthHandler
+import datetime
+import sqlite3
+
+SQLconn = sqlite3.connect('passport.sqlite')
+SQL = SQLconn.cursor()
+SQL.execute("""
+        create table if not exists passport (
+            tweet   text,
+            id      text primary key,
+            sent    text);
+          """)
+
 
 consumer_key = os.environ['CKEY']
 consumer_secret = os.environ['CSRT']
@@ -13,11 +24,7 @@ auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
 
-user = api.me()
- 
-print('Name: ' + user.name)
-print ()
-
+'''
 # Get the User object for twitter...
 user = api.get_user('jameshaley')
 print (user.screen_name)
@@ -25,10 +32,49 @@ print (user.followers_count)
 for friend in user.friends():
    print (friend.screen_name)
 print()
-
-tweets = api.user_timeline(screen_name = 'SpaceX', count = 100)
+'''
+tweets = api.user_timeline(screen_name = 'SpaceX', count = 10)
+#print (dir(tweets[0]))
 
 for chirp in tweets:
-    #print (dir(chirp))
+
     if '@elonmusk' in chirp.text:
-        print (chirp.text)
+        print ('text      = '+chirp.text)
+        print ('id        = '+chirp.id_str)
+        print ('created_at= '+chirp.created_at.strftime('%y/%m/%d %H:%M:%S'))
+        print ()
+        SQL.execute("""INSERT or IGNORE into passport 
+                        values (?,?,?)""", 
+                       (chirp.text, 
+                        chirp.id_str,
+                        chirp.created_at.strftime('%y/%m/%d %H:%M:%S')))
+        SQLconn.commit()
+
+
+SQLconn.close()
+
+status = api.rate_limit_status()
+print ('User Timeline Remaining: ', end='')
+#print (status['resources']['statuses']['/statuses/user_timeline'])
+print (status['resources']['statuses']['/statuses/user_timeline']['remaining'], ' ', end='')
+print (datetime.datetime.fromtimestamp(
+        status['resources']['statuses']['/statuses/user_timeline']['reset']
+        ).strftime('%Y-%m-%d %H:%M:%S'))
+print ('Application Remaining: ', end='')
+#print (status['resources']['application']['/application/rate_limit_status'])
+print (status['resources']['application']['/application/rate_limit_status']['remaining'],  ' ', end='')
+print (datetime.datetime.fromtimestamp(
+        status['resources']['application']['/application/rate_limit_status']['reset']
+        ).strftime('%Y-%m-%d %H:%M:%S'))
+'''for s in status:
+    #print(s)
+    if s == 'rate_limit_context': continue
+    for x in status[s]:
+        #print (x,':',status[s][x] )
+        print ()
+        for y in status[s][x]:
+            print (s,':',x,':',y,':')
+            print (status[s][x][y])
+            print ()
+            a = input("")
+'''
